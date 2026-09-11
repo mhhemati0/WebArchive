0cd420f240850cb2af4fec31c1e23f445acf1956
MH Hemati <mhhemati0@gmail.com>
2026-09-11 11:34:19 +0330

    Fixing the errors

--- src/main.py
+++ src/main.py
@@ -3,11 +3,10 @@ import xml.etree.ElementTree as ET
 from pathlib import Path
 from libzim.reader import Archive
 from libzim.search import Query, Searcher
-from .webarchive.setup import *
-from .webarchive.kiwix_lib import KiwixLibraryDialog
-from .webarchive.functions import *
-from .webarchive.app import *
-from gi.repository import Gtk, Adw, GLib,Gio
+from .state import *
+from .kiwix_lib import KiwixLibraryDialog
+from .app import *
+from gi.repository import Gtk, Adw, GLib,Gio,Gdk
 icon_theme = Gtk.IconTheme.get_for_display(Gdk.Display.get_default())
 icon_theme.add_resource_path("/io/github/mhhemati0/WebArchive")
 def main(version=None):
